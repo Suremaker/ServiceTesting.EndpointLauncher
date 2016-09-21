9936420bcae9f0ffd8536cc8abd55ee86dc3c7a6
Suremaker <wojciech.kotlarski@gmail.com>
2016-09-21 20:11:39 +0100

    Upload reports to appveyor

--- make/Makefile.ps1
+++ make/Makefile.ps1
@@ -10,11 +10,16 @@ Define-Step -Name "Build solution" -Target "DEV,BUILD" -Body {
 	call "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Wonga.ServiceTesting.EndpointLauncher.sln /t:"Clean,Build" /p:Configuration=Release /m /verbosity:m /nologo /p:TreatWarningsAsErrors=true /tv:14.0
 }
 
-Define-Step -Name "Tests" -Target "DEV,BUILD" -Body {
+Define-Step -Name "Test" -Target "DEV,BUILD" -Body {
 	. (require 'psmake.mod.testing')
 
 	Define-NUnit3Tests -GroupName 'Tests' -ReportName 'test-results' -TestAssembly '*\bin\Release\*.Tests.dll' `
 		| Run-Tests -EraseReportDirectory -ReportDirectory "reports"
+}
+
+Define-Step -Name "Upload test results" -Target "BUILD" -Body {
+	$wc = New-Object 'System.Net.WebClient'
+	$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\reposts\test-results.xml))
 }
 
 Define-Step -Name "Package" -Target "DEV,BUILD" -Body {
